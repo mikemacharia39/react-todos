3d811610d5d66701797628817f95b47585ccb32c
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-14 00:05:51 +0300

    learning react

--- src/App.js
+++ src/App.js
@@ -3,21 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        Your Tasks
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th scope='col'>
+                #
+              </th>
+              <th scope='col'>
+                Task
+              </th>
+              <th scope='col'>
+                Description
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+              <tr>
+              <th scope='row'>1</th>
+              <td>Learn React</td>
+              <td>React is a library for developing modern single page websites</td>
+              </tr>
+              <tr>
+              <th scope='row'>2</th>
+              <td>Meeting</td>
+              <td>Attend meeting at 10:30am</td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
