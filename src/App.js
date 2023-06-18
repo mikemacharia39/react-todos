bb9e2d067768dc16cb0d423bf6007315f6f1f11b
mikemacharia39 <mikemacharia39@gmail.com>
2023-06-18 21:15:45 +0300

    reference form in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'; // adds state functionality in our component
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
@@ -35,6 +36,7 @@ function App() {
           <button className='btn btn-primary' onClick={showTodo}>
             Add New Todo
           </button>
+          <NewTodoForm/>
         </div>
       </div>
     </div>
